c5ee24ff35f79d99693f34f2ddfdbc5cbdd30eaa
Prashuk Ajmera <prashuk.ajmera@gmail.com>
2025-04-07 15:51:38 -0700

    Inital Commit

---
+++ iOS-CiCd-CodeMagic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS-CiCd-CodeMagic
+//
+//  Created by Prashuk Ajmera on 4/7/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ iOS-CiCd-CodeMagicUITests/iOS_CiCd_CodeMagicUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  iOS_CiCd_CodeMagicUITestsLaunchTests.swift
+//  iOS-CiCd-CodeMagicUITests
+//
+//  Created by Prashuk Ajmera on 4/7/25.
+//
+
+import XCTest
+
+final class iOS_CiCd_CodeMagicUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
